e9225119a29101269c9be29c78798661a4a1e379
Pierre Jeambrun <pierre606@hotmail.fr>
2018-01-03 13:51:10 +0100

    Fix import

--- training/sent_cnn_model/train_cnn.py
+++ training/sent_cnn_model/train_cnn.py
@@ -6,7 +6,7 @@ import os
 import sys
 sys.path.append(os.path.normpath(os.path.join(os.path.abspath(__file__), "../../../")))
 import preprocessing.sent_cnn_data_helpers as dh
-from sent_cnn_1d import SentCNN
+from sent_cnn import SentCNN
 import config
 
 def load_data(config):
